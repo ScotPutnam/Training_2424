86014cfd74489b5f5883310dbde0c223dbd39ed4
ScotPutnam <scot.putnam@my.uwrf.edu>
2026-06-16 11:28:23 -0500

    vs

---
+++ Week3/Day2/p0demo/src/main/java/com/scotputnam/utils/ConnectionUtil.java
@@ -0,0 +1,22 @@
+package com.scotputnam.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+    // eventually returns object of type Connection
+    // used to interact with our DB
+    public static Connection getConnection() throws SQLException{
+        try{
+            Class.forName("org.sqlite.JDBC");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+            System.out.println("Problem occurred locating driver");
+        }
+        String url = "jdbc:sqllite:src/dbfile/dbfile";
+        return DriverManager.getConnection(url);
+    }
+}
