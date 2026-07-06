1883db33670c30cc466273472d60f71acaf6b855
ScotPutnam <scot.putnam@my.uwrf.edu>
2026-07-06 09:33:07 -0500

    local

--- Week6/examples/Junit6_demo/src/test/java/com/revature/unittest/Junit6_demo_basics.java
+++ Week6/examples/Junit6_demo/src/test/java/com/revature/unittest/Junit6_demo_basics.java
@@ -1,4 +1,29 @@
 package com.revature.unittest;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// provides human readable test name for reports
+@DisplayName("Basic unit tests")
 public class Junit6_demo_basics {
+    // AAA pattern - Arrange, Act, Assert
+    // System under test
+
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    @DisplayName("Adding two positive numbers"){
+        // ARRANGE - Set up the test data
+        int a = 1;
+        int b = 2;
+
+        // ACT - Execute the method under test
+        int result = calculator.add(a, b);
+
+        // ASSERT -
+        assertEquals()
+    }
+
+
 }
